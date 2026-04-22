d1ac81769659c27e28a5d2c1be34cb69d2ec5226
PhamHoNamKhanh <khanhmatacc2007@gmail.com>
2026-04-22 12:42:46 +0700

    Ex5

--- Ex5.sql
+++ Ex5.sql
@@ -1,30 +1,29 @@
-create database marketing;
-use marketing;
-
-create table Users(
-	cus_name varchar (100) not null ,
-    cus_mail varchar (100) primary key,
-    cus_total_orders int check (cus_total_orders > 0) not null 
-);
-INSERT INTO Users (cus_name, cus_mail, cus_total_orders) VALUES
-('Nguyễn Văn An',     'an.nguyen@gmail.com',      50),
-('Trần Thị Bích',     'bich.tran@yahoo.com',       120),
-('Lê Hoàng Nam',      'nam.le@outlook.com',         300),
-('Phạm Minh Châu',    'chau.pham@gmail.com',        800),
-('Hoàng Thị Dung',    'dung.hoang@hotmail.com',     10000),
-('Vũ Quốc Hùng',      'hung.vu@gmail.com',          200),
-('Đặng Thị Lan',      'lan.dang@yahoo.com',          70),
-('Bùi Văn Mạnh',      'manh.bui@gmail.com',          40),
-('Ngô Thị Hoa',       'hoa.ngo@outlook.com',         150),
-('Trịnh Công Sơn',    'son.trinh@gmail.com',          90);
-select cus_name,
-	case 
-		when cus_total_orders < 100 then 'Bac'
-        when cus_total_orders between 100 and 500 then 'Vang'
-        else 'Kim cuong'
-        end as cus_rank
-from users 
-
 /*
-- ràng buộc orders dạng int kèm điều kiện lớn hơn 0 và không trống
+Khắc phục lỗi null: 
+sử dụng left join kết hợp với is null 
 */
+create database Inventory;
+use inventory;
+create table rooms(
+room_id int primary key auto_increment,
+room_name varchar(50) not null
+);
+create table bookings(
+room_id int,
+foreign key(room_id) references rooms(room_id),
+id int primary key auto_increment,
+descriptions varchar(50)
+);
+INSERT INTO rooms (room_name) VALUES 
+('Phòng A'), ('Phòng B'), ('Phòng C'), ('Phòng D');
+INSERT INTO bookings (room_id, descriptions) VALUES 
+(1, 'Booking 1'),
+(1, 'Booking 2'),
+(2, 'Booking 3'),
+(1, 'Booking 4'),
+(NULL, 'Booking ẩn danh 1'),
+(NULL, 'Booking ẩn danh 2');
+select *
+from rooms as r left join bookings as b
+on r.room_id = b.room_id
+where b.room_id is null;
